1f45d712253da76231114449e4d1a9754f69a23a
ryderbrooks <ryderbrooks@gmail.com>
2019-04-25 09:43:31 -0400

    refactor

--- src/lib/Threshold.ts
+++ src/lib/Threshold.ts
@@ -2,36 +2,46 @@ import { IThreshold }                from '../meta/interfaces';
 import { sThrottleThresholdOptions } from '..';
 
 
-function randInt(min: number, max: number): number{
-    return Math.floor(Math.random()*(max-min+1)+min );
+function randInt( min: number, max: number ): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+
+
 export class Threshold implements IThreshold {
-    public constructor(thresholds: sThrottleThresholdOptions){
-        this.thresholds = thresholds;
-    }
-    private thresholds: sThrottleThresholdOptions;
+  public get reqDelay(): number {
+    return randInt(this.thresholds.minReqPause, this.thresholds.maxReqPause);
+  }
 
-    public get reqDelay(): number{
-        return randInt(this.thresholds.minReqPause, this.thresholds.maxReqPause);
-    }
-    public get sleepDelay(): number{
-        return randInt(this.thresholds.minSleepMS, this.thresholds.maxSleepMS);
-    }
 
-    public get maxTotalReqs(): number{
-        return this.thresholds.maxTotalReqs;
-    }
+  public get sleepDelay(): number {
+    return randInt(this.thresholds.minSleepMS, this.thresholds.maxSleepMS);
+  }
 
-    public shouldDelayReq(lastReqTime: number): boolean{
-        return Date.now() - lastReqTime < this.reqDelay;
-    }
 
-    public shouldSleep(reqCnt: number): boolean{
-        return reqCnt >= randInt(
-            this.thresholds.minReqsPerSleep,
-            this.thresholds.maxReqsPerSleep);
-    }
+  public get maxTotalReqs(): number {
+    return this.thresholds.maxTotalReqs;
+  }
+
+
+  public shouldDelayReq( lastReqTime: number ): boolean {
+    return Date.now() - lastReqTime < this.reqDelay;
+  }
+
+
+  public shouldSleep( reqCnt: number ): boolean {
+    return reqCnt >= randInt(
+      this.thresholds.minReqsPerSleep,
+      this.thresholds.maxReqsPerSleep);
+  }
+
+
+  public constructor( thresholds: sThrottleThresholdOptions ) {
+    this.thresholds = thresholds;
+  }
+
+
+  private thresholds: sThrottleThresholdOptions;
 }
 
 
